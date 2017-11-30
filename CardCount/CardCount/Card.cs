84214fd4c6301ef116028da886d5afeff8de2e75
S3TO <ninja00000001@gmail.com>
2017-11-29 20:00:07 -0800

    all files
    all files

---
+++ CardCount/CardCount/Card.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace CardCount
+{
+    class Card : GameObject
+    {
+        protected string suitN;
+        protected string valueN;
+        public int value;
+        public int suit;// 1 = clubs, 2 = diamonds, 3 = hearts, 4 = spades
+        public Card(int suit, int value, Game myGame) : base(myGame)
+        {
+            this.value = value;
+            this.suit = suit;
+            switch (suit)
+            {
+                case 1: suitN = "c"; break;
+                case 2: suitN = "d"; break;
+                case 3: suitN = "h"; break;
+                case 4: suitN = "s"; break;
+                default: break;
+            }
+            switch (value)
+            {
+                case 1: valueN = "01"; break;
+                case 2: valueN = "02"; break;
+                case 3: valueN = "03"; break;
+                case 4: valueN = "04"; break;
+                case 5: valueN = "05"; break;
+                case 6: valueN = "06"; break;
+                case 7: valueN = "07"; break;
+                case 8: valueN = "08"; break;
+                case 9: valueN = "09"; break;
+                case 10: valueN = "10"; break;
+                case 11: valueN = "11"; break;
+                case 12: valueN = "12"; break;
+                case 13: valueN = "13"; break;
+                default: break;
+            }
+            textureName = "c01";
+        }
+        public override void Update(float deltaTime)
+        {
+            base.Update(deltaTime);
+        }
+        public override void Draw(SpriteBatch batch)
+        {
+            if (texture != null)
+            {
+                Vector2 drawPosition = position;
+                batch.Draw(texture, drawPosition, Color.White);
+            }
+        }
+    }
+}
---
+++ CardCount/CardCount/Game1.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+namespace CardCount
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        List<Card> deck = new List<Card>();
+        Card card;
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            graphics.PreferredBackBufferHeight = 768;
+            graphics.PreferredBackBufferWidth = 1063;
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            for(int i = 1;i < 5; i++)
+            {
+                for(int j = 1; j < 14; j++)
+                {
+                    Card temp = new Card(i, j, this);
+                    temp.LoadContent();
+                    temp.position = new Vector2(0+j*768,0+i*1063);
+                    deck.Add(temp);
+                }
+            }
+            card = new Card(1, 1, this);
+            card.LoadContent();
+            card.position = new Vector2(0, 0);
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            card.Draw(spriteBatch);
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
